69face513d7b3da27bcb9164fdc751dc26e01b96
CARLOS DÍAZ <diazromerocarlos2@gmail.com>
2024-11-23 13:54:41 +0000

    creacion de dist

--- vite.config.js
+++ vite.config.js
@@ -5,3 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
 })
+//dist(assets(index-Ca449wdD.css y i9ndex-D8bA-9WG.js), imagenes e index.html)
+//node_modules
+//public (imagenes (se duplicaron en dist))
+//src(assets(logoreact.svg), components(player.jsx, player.css), app.css, app.jsx, index.css, main.jsx)
